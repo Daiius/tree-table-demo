b2429a37cdeccac87376d8bb3fd46ac119cd6b0f
Daiji Yamashita <yamashita.daiji@gmail.com>
2023-08-14 01:57:35 +0900

    Add Dockerfile and script for mypy check

---
+++ webapi/check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+name=tree-table-demo-webapi-check
+
+docker build -t $name -f Dockerfile.check .
+
+docker run --name $name $name
+
+# copy files from check container if necesarry...
+# docker cp $name:/check
+
+docker container rm $name
