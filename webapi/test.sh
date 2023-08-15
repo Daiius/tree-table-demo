456511ae44261b72995f8c3fb41f2700c63cd87e
Daiji Yamashita <yamashita.daiji@gmail.com>
2023-08-16 01:07:04 +0900

    add test scripts

---
+++ webapi/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+compose_file=docker-compose-test.yml
+
+set -e
+docker compose -f $compose_file build
+set -e
+
+docker compose -f $compose_file up --exit-code-from=tree-table-demo-webapi-test
+
+# copy necessary files
+
+docker compose -f $compose_file down
+
