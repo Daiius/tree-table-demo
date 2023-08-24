d8e841931119045494e2ab1af3a10492679fd6d4
Daiji Yamashita <yamashita.daiji@gmail.com>
2023-08-25 01:19:36 +0900

    add basic read and show functions

--- interface/build.sh
+++ interface/build.sh
@@ -2,9 +2,10 @@
 
 set -e
 docker build -t tree-table-demo-interface-build -f Dockerfile.build .
+set +e
 
-docker run --name tree-table-demo-interface-build tree-table-demo-interface-build
+docker run -t --name tree-table-demo-interface-build tree-table-demo-interface-build
 
-docker cp tree-table-demo-interface-build:/build/dist ./dist
+docker cp tree-table-demo-interface-build:/build/dist .
 
 docker container rm tree-table-demo-interface-build
