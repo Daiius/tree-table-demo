90bc3179efcb5f003637602fc4e8e14dd8237297
Daiji Yamashita <yamashita.daiji@gmail.com>
2023-10-29 01:29:15 +0900

    add scss stype sheets

--- webserver/src/main.tsx
+++ webserver/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import MainView from './MainView';
 
+import './main.scss';
+
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 root.render(<MainView/>);
--- webserver/webpack.config.js
+++ webserver/webpack.config.js
@@ -10,11 +10,30 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modues/,
-      },
+      }, {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: () => require('autoprefixer')
+            }
+          }
+        }, {
+          loader: 'sass-loader'
+        }]
+      }, {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        type: "asset/inline"
+      }
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', '.woff', '.woff2', '.css', '.scss'],
   },
   output: {
     filename: 'bundle.js',
