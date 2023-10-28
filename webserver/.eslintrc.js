297c5b43d01d17465f9b6ddd0dd774d50911edbc
Daiji Yamashita <yamashita.daiji@gmail.com>
2023-10-28 23:13:02 +0900

    add tsc --noEmit and tsc eslint docker script

---
+++ webserver/.eslintrc.js
@@ -0,0 +1,23 @@
+// https://qiita.com/notakaos/items/85fd2f5c549f247585b1 
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2019,
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json' ]
+  },
+  extends: [
+    'eslinnt:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+  },
+};
+
