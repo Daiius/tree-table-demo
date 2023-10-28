3f220ae0522b9aaa424d1b1e38f52b0133b2cf94
Daiji Yamashita <yamashita.daiji@gmail.com>
2023-10-29 01:55:40 +0900

    fix return code of shell script

--- webserver/build.sh
+++ webserver/build.sh
@@ -5,7 +5,10 @@ docker build -t tree-table-demo-interface-build -f Dockerfile.build .
 set +e
 
 docker run -t --name tree-table-demo-interface-build tree-table-demo-interface-build
+result=$?
 
 docker cp tree-table-demo-interface-build:/build/dist .
 
 docker container rm tree-table-demo-interface-build
+
+exit $result
