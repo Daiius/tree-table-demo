8d86be329bded88f9b245d6fdc6d4da96d6018b8
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-02-04 15:19:47 +0900

    fix container name, service name, etc

--- webapi/check.sh
+++ webapi/check.sh
@@ -4,7 +4,7 @@ name=tree-table-demo-webapi-check
 
 docker build -t $name -f Dockerfile.check .
 
-docker run --name $name $name
+docker run -t --name $name $name
 
 # copy files from check container if necesarry...
 # docker cp $name:/check
